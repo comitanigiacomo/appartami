0b92965ceedf835c69a89d3ff1a98a43d388c94b
GiacomoComitani <giacomo.comitani@studenti.unimi.it>
2024-04-24 15:56:33 +0200

    add dotenv dipendence

--- backend/src/index.js
+++ backend/src/index.js
@@ -4,20 +4,25 @@ const app = express();
 
 const PORT = process.env.PORT || 8080; 
 
-const userRoutes = require('./routes/userRoutes.js');
-const apartmentRoutes = require('./routes/apartmentRoutes.js');
-const preferenceRoutes = require('./routes/preferenceRoutes.js');
+//const userRoutes = require('./routes/userRoutes.js');
+//const apartmentRoutes = require('./routes/apartmentRoutes.js');
+//const preferenceRoutes = require('./routes/preferenceRoutes.js');
 
 //Connessione al database: 
 connectToMongoDB()
   .then(database => {
-
+    
     // Middleware per il parsing del corpo delle richieste
     app.use(express.json());
 
+    /*
+
     app.use('/api/users', userRoutes);
+    
     app.use('/api/preferences', preferenceRoutes);
     app.use('/api/apartments', apartmentRoutes)
+
+    */
 
     // Esegui il server Express solo dopo aver stabilito la connessione al database
     app.listen(PORT, () => {
