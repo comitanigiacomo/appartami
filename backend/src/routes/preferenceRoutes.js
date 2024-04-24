76349631ffa5fc3d3fbe6a3ef4daed31d788766d
GiacomoComitani <giacomo.comitani@studenti.unimi.it>
2024-04-24 15:19:51 +0200

    backend : src/routes/preferenceRoutes: add explanations of functions

--- backend/src/routes/preferenceRoutes.js
+++ backend/src/routes/preferenceRoutes.js
@@ -2,8 +2,13 @@ const express = require('express');
 const router = express.Router();
 const PreferenceController = require('../controllers/PreferenceController');
 
+// Ottiene le preferenze
 router.get('/', PreferenceController.getPreferences);
+
+// Aggiunge una nuova preferenza
 router.post('/', PreferenceController.addPreference);
+
+// Elimina una preferenza
 router.delete('/:id', PreferenceController.deletePreference);
 
 module.exports = router;
