aa68087157ea93e39abe58961bc04ac823211dee
GiacomoComitani <giacomo.comitani@studenti.unimi.it>
2024-04-24 15:10:59 +0200

    backend : update preferenceRoutes

--- backend/src/routes/preferenceRoutes.js
+++ backend/src/routes/preferenceRoutes.js
@@ -1,12 +1,9 @@
 const express = require('express');
 const router = express.Router();
+const PreferenceController = require('../controllers/PreferenceController');
 
-const preferenceController = require('../controllers/preferenceController');
-
-router.get('/', preferenceController.getAllPreferences);
-router.get('/:id', preferenceController.getPreferenceById);
-router.post('/', preferenceController.createPreference);
-router.put('/:id', preferenceController.updatePreference);
-router.delete('/:id', preferenceController.deletePreference);
+router.get('/', PreferenceController.getPreferences);
+router.post('/', PreferenceController.addPreference);
+router.delete('/:id', PreferenceController.deletePreference);
 
 module.exports = router;
