3efcae5cc75865cab792b7573c511bb2f73186c3
GiacomoComitani <giacomo.comitani@studenti.unimi.it>
2024-04-25 15:48:14 +0200

    backend : /src/index.js : refactoring connection to databse

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,28 +1,24 @@
 const express = require("express"); 
+
+const userRoutes = require('./routes/userRoutes.js');
+const apartmentRoutes = require('./routes/apartmentRoutes.js');
+const preferenceRoutes = require('./routes/preferenceRoutes.js');
+
 const { connectToMongoDB } = require('./database/config.js');
 const app = express(); 
 
 const PORT = process.env.PORT || 8080; 
 
-//const userRoutes = require('./routes/userRoutes.js');
-//const apartmentRoutes = require('./routes/apartmentRoutes.js');
-//const preferenceRoutes = require('./routes/preferenceRoutes.js');
-
 //Connessione al database: 
 connectToMongoDB()
-  .then(database => {
+  .then(() => {
     
     // Middleware per il parsing del corpo delle richieste
     app.use(express.json());
 
-    /*
-
     app.use('/api/users', userRoutes);
-    
     app.use('/api/preferences', preferenceRoutes);
     app.use('/api/apartments', apartmentRoutes)
-
-    */
 
     // Esegui il server Express solo dopo aver stabilito la connessione al database
     app.listen(PORT, () => {
