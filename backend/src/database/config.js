e57f3b9d01e15bcb3e897936bc1c462f6fd0394b
GiacomoComitani <giacomo.comitani@studenti.unimi.it>
2024-04-25 15:51:22 +0200

    backend : /src/database/config.js : refactoring connection to use only mongoose

--- backend/src/database/config.js
+++ backend/src/database/config.js
@@ -1,20 +1,15 @@
-const { MongoClient } = require("mongodb");
+const mongoose = require('mongoose');
 require('dotenv').config();
 
 const uri = process.env.MONGODB_URI;
 
-const client = new MongoClient(uri);
-
 async function connectToMongoDB() {
   try {
-
-    await client.connect();
+    await mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true });
     console.log("Connected successfully to MongoDB server");
-    return client.db();
-
   } catch (err) {
-    console.log("Error connecting to MongoDB:", err);
-    throw err; 
+    console.error("Error connecting to MongoDB:", err);
+    throw err;
   }
 }
 
