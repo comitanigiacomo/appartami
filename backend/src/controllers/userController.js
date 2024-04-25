eb75ec3c736bfb28c5634d5c5a5e3f02b26d771f
GiacomoComitani <giacomo.comitani@studenti.unimi.it>
2024-04-25 23:09:09 +0200

    backend : update user model

--- backend/src/controllers/userController.js
+++ backend/src/controllers/userController.js
@@ -15,8 +15,8 @@ exports.registerUser = async (req, res) => {
 // Controller per il login degli utenti
 exports.loginUser = async (req, res) => {
   try {
-    const { email, password } = req.body;
-    const user = await User.findOne({ email, password });
+    const { username, password } = req.body;
+    const user = await User.findOne({ username, password });
     if (!user) {
       return res.status(404).json({ error: 'User not found' });
     }
