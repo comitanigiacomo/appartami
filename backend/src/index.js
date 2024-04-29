7e28c63f1c03b9d95fe75296e0df5e32bd41a5d9
GiacomoComitani <giacomo.comitani@studenti.unimi.it>
2024-04-29 12:43:19 +0200

    backend: src/index.js : fix connection to db

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,26 +1,28 @@
 const express = require("express"); 
+const { connectToMongoDB } = require('./database/config.js');
+const app = express(); 
 
+// Importa le route
 const userRoutes = require('./routes/userRoutes.js');
 const apartmentRoutes = require('./routes/apartmentRoutes.js');
 const preferenceRoutes = require('./routes/preferenceRoutes.js');
 const userDistributionRoutes = require('./routes/userDistributionRoutes.js');
-
-const { connectToMongoDB } = require('./database/config.js');
-const app = express(); 
+const authRoutes = require('./routes/authRoutes.js');
 
 const PORT = process.env.PORT || 8080; 
 
-//Connessione al database: 
+// Connessione al database
 connectToMongoDB()
   .then(() => {
     
     // Middleware per il parsing del corpo delle richieste
     app.use(express.json());
 
-
+    // Aggiungi le route
     app.use('/api/users', userRoutes);
     app.use('/api/preferences', preferenceRoutes);
     app.use('/api/apartments', apartmentRoutes);
+    app.use('/api/auth', authRoutes); 
     app.use('/api', userDistributionRoutes);
 
     // Esegui il server Express solo dopo aver stabilito la connessione al database
