ce35ab395cc3410b3276c369e2fafe6c8268786b
GiacomoComitani <giacomo.comitani@studenti.unimi.it>
2024-04-30 14:14:33 +0200

    fix

--- backend/src/routes/authRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const authController = require('../controllers/authController');
-const router = express.Router();
-
-// Rotta per il login degli utenti
-router.post('/login', authController.login);
-
-module.exports = router;
--- backend/src/routes/userDistributionRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const userDistributionController = require('../controllers/userDistributionController');
-
-// Route per distribuire gli utenti negli appartamenti
-router.post('/distribute-users', userDistributionController.distribute);
-
-module.exports = router;
