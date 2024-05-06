f910e9e1219526ba1674a3eed45cb6d20851926e
GiacomoComitani <giacomo.comitani@studenti.unimi.it>
2024-05-06 14:19:07 +0200

    fix

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,4 +1,16 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {}
 
-module.exports = nextConfig
+module.exports = {
+    nextConfig,
+    async redirects() {
+        return [
+          {
+            source: '/Inizia-ora',
+            destination: '/users',
+            permanent: true,
+          },
+        ];
+      },
+
+}
