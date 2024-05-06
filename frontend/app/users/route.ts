009a937f3e9b0269c2677af27985802389b7ae8d
GiacomoComitani <giacomo.comitani@studenti.unimi.it>
2024-05-06 16:07:24 +0200

    delete frontend

--- frontend/app/users/route.ts
+++
@@ -1,8 +0,0 @@
-// app/users/route.ts
-import { NextResponse } from "next/server";
-
-export async function GET(request: Request) {
-    // Fai ciò che desideri con la richiesta
-    // Ad esempio, restituisci una risposta con il contenuto della pagina degli utenti
-    return new NextResponse("<h1>Benvenuti nella pagina degli utenti!</h1>");
-}
